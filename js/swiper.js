d73ab280763b53a36394c9c4942f5232fbb19d37
pocachip <icanworkitout21@gmail.com>
2021-10-07 05:39:02 +0900

    :construction:index.html: main-banner(Swiper) 기본 작업

---
+++ js/swiper.js
@@ -0,0 +1,40 @@
+// new Swiper('.swiper-container', {
+//   pagination: {
+//     // 페이징 설정
+//     el: '.swiper-pagination',
+//     clickable: true, // 페이징을 클릭하면 해당 영역으로 이동, 필요시 지정해 줘야 기능 작동
+//   },
+//   navigation: {
+//     // 네비게이션 설정
+//     nextEl: '.swiper-button-next', // 다음 버튼 클래스명
+//     prevEl: '.swiper-button-prev', // 이번 버튼 클래스명
+//   },
+// });
+
+const swiper = new Swiper('.swiper-container--main-banner', {
+  slidesPerView: 1, // 한 화면에 들어오는 슬라이드 갯수
+  spaceBetween: 0, // 슬라이드 간 여백
+  debugger: true, // 드래그로 슬라이드 이동
+  mousewheel: true,
+  loop: true,
+  // loopAdditionalSlides: 1, // loop 생성 후 복제할 슬라이드 수 추가
+  effect: 'fade', // 슬라이드 이동 효과 'slide', 'fade'
+  /* fadeEffect: { // effect: 'fade' 설정
+    crossFade: true,
+  }, */
+  speed: 1000, // 슬라이드 속도
+  // 슬라이드 이동 자동 실행
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false, // 수동 이동 시 autoplay 중지
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets', // 'bullets', 'custom'
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
