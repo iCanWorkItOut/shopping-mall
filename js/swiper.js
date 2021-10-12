395389642f4b6ccc679af9073d2fa38f719e32c8
pocachip <icanworkitout21@gmail.com>
2021-10-13 02:30:50 +0900

    :wrench:index.html: bottom-banner

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper-container--main-banner', {
+const swiperMain = new Swiper('.swiper-container--main-banner', {
   slidesPerView: 1, // 한 화면에 들어오는 슬라이드 갯수
   spaceBetween: 0, // 슬라이드 간 여백
   debugger: true, // 드래그로 슬라이드 이동
@@ -16,12 +16,40 @@ const swiper = new Swiper('.swiper-container--main-banner', {
     disableOnInteraction: false, // 수동 이동 시 autoplay 중지
   },
   pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets', // 'bullets', 'custom'
+    el: '.swiper-pagination--main-banner',
+    type: 'bullets', // 'bullets', 'fraction', 'custom'
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next--main-banner',
+    prevEl: '.swiper-button-prev--main-banner',
   },
+});
+
+const swiperBottom = new Swiper('.swiper-container--bot-banner', {
+  slidesPerView: 2, // 한 화면에 들어오는 슬라이드 갯수
+  spaceBetween: 20, // 슬라이드 간 여백
+  debugger: true, // 드래그로 슬라이드 이동
+  // mousewheel: true,
+  loop: true,
+  // loopAdditionalSlides: 1, // loop 생성 후 복제할 슬라이드 수 추가
+  effect: 'slide', // 슬라이드 이동 효과 'slide', 'fade'
+  /* fadeEffect: { // effect: 'fade' 설정
+    crossFade: true,
+  }, */
+  speed: 700, // 슬라이드 속도
+  // 슬라이드 이동 자동 실행
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false, // 수동 이동 시 autoplay 중지
+  },
+  pagination: {
+    el: '.swiper-pagination--bot-banner',
+    type: 'fraction', // 'bullets', 'fraction', 'custom'
+    clickable: true,
+  },
+  // navigation: {
+  //   nextEl: '.swiper-button-next--bot-banner',
+  //   prevEl: '.swiper-button-prev--bot-banner',
+  // },
 });
